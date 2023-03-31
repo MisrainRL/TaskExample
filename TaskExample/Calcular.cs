54d2b2beb6470d656ac6d1fb87fe8fd27b24424f
MisrainRL <80868507+MisrainRL@users.noreply.github.com>
2023-03-31 16:13:34 -0600

    Agregar archivos de proyecto.

---
+++ TaskExample/Calcular.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExample
+{
+    public class Calcular
+    {
+        public object locked = new object();
+        public int Add { get; set; }   
+        public int Add2 { get; set; }
+
+        public int Balance { get; set; }
+
+        public void Deposit(int amount)
+        {
+            lock (locked)
+            {
+                Balance += amount;
+            }
+        }
+
+        public void Draw(int amount)
+        {
+            lock (locked)
+            {
+                Balance -= amount;
+            }
+        }
+        public void AddNum(int num)
+        {
+            lock (locked)
+            {
+                Add += num;
+            }
+        }
+
+        public void AddNum2(int num)
+        {
+          
+            lock (locked)
+            {
+                
+                Add2 *= num;
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskExample;
+
+namespace Test
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        static async Task Main(string[] args)
+        {
+            var sum = new Calcular();
+            var task = new Task(() =>
+            {
+                Console.WriteLine("Comenzamos a realizar la tarea interna del task");
+                //Comenzamos a sumar los primeros 100 numeros con un intervalo de espera de medio segundo
+                for (int i = 1; i <= 100; i++)
+                {
+                    Thread.Sleep(100);
+                    sum.AddNum(i);
+                }
+                Console.WriteLine("Termino la tarea 1");
+                Console.WriteLine($"La suma final es {sum.Add}");
+        });
+
+            var task2 = new Task(() =>
+            {
+                Console.WriteLine("Comenzamos a realizar la tarea interna del task2");
+                //Comenzamos a sumar los primeros 100 numeros con un intervalo de espera de medio segundo
+                for (int i = 1; i <= 100; i++)
+                {
+                    Thread.Sleep(50);
+                    sum.AddNum(i);
+                }
+                Console.WriteLine("Termino la tarea 2");
+                Console.WriteLine($"La suma final es {sum.Add}");
+            });
+            ////////////////////////////////////////////////////////
+            List<Task> tasksL = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasksL.Add(Task.Factory.StartNew(() =>
+                { 
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        sum.Deposit(100);
+                    }
+                }));
+
+                tasksL.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int k = 0; k < 1000; k++)
+                    {
+                        sum.Draw(100);
+                    }
+                }));
+            }
+        
+            task.Start();
+            task2.Start();
+            await task;
+            await task2;
+
+            Task.WaitAll(tasksL.ToArray());
+
+            Console.WriteLine($"Task count {tasksL.Count}");
+
+            Console.WriteLine($"Final balance is {sum.Balance}");
+            Console.WriteLine("Termine");
+
+            Console.ReadLine();
+        }
+    }
+}
